e4342399dbc4c8b1b7fa84a54a04b32ccce90564
m0_yasser <git config --global user.email you@example.com>
2023-08-22 15:49:13 -0700

    correcting errors

--- string_fields.c
+++ string_fields.c
@@ -21,7 +21,7 @@ char *get_precision(char *p, params_t *params, va_list ap)
 	}
 	else
 	{
-		while (_sidigit(*p))
+		while (_isdigit(*p))
 			d = d * 10 + (*p++ - '0');
 	}
 	params->precision = d;
