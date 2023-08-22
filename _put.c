47da5bb170a38d3bb0d4542ee58273aaa2d29144
MoustafaAmer12 <moustafa3sam20113@gmail.com>
2023-08-22 16:07:33 -0700

    _put.c edit

--- _put.c
+++ _put.c
@@ -27,7 +27,7 @@ int _putchar(int c)
 	static int i;
 	static char buf[OUTPUT_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= OUTPUT_BUF_FLUSH)
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
 	{
 		write(1, buf, i);
 		i = 0;
