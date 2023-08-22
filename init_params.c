f25b49ff1d9e11fabe9084f72bb7b937dccf46c1
MoustafaAmer12 <moustafa3sam20113@gmail.com>
2023-08-22 15:43:32 -0700

    Edits

--- init_params.c
+++ init_params.c
@@ -12,13 +12,13 @@ void init_parameter(params_t *params, va_list ap)
 	params->unsign = 0;
 
 	params->plus_flag = 0;
-	params->space_falg = 0;
+	params->space_flag = 0;
 	params->hashtag_flag = 0;
 	params->zero_flag = 0;
 	params->minus_flag = 0;
 
 	params->width = 0;
-	params->percision = UNIT_MAX;
+	params->precision = UINT_MAX;
 
 	params->h_modifier = 0;
 	params->l_modifier = 0;
